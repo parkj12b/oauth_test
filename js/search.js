5311bd45fa3cf3c08cff2a9ca6938a4e9447f51c
Minseo Park <minsepar@c1r3s7.42seoul.kr>
2024-07-31 14:34:35 +0900

    .

--- js/search.js
+++ js/search.js
@@ -1,6 +1,3 @@
-import algoliasearch from 'algoliasearch/lite';
-import instantsearch from 'instantsearch.js/es';
-
 const searchClient = algoliasearch('D92FAAH3QP', '5d528996ae02dbe0b55596d1736f1ed0');
 
 const search = instantsearch({
