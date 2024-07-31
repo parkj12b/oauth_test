4da6040a08ba7efe1dd526c80c660db70564befd
Minseo Park <minsepar@c1r3s7.42seoul.kr>
2024-07-31 14:07:11 +0900

    .

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from 'https://cdn.jsdelivr.net/npm/algoliasearch@4.24.0/dist/algoliasearch-lite.umd.js';
 import instantsearch from 'instantsearch.js/es';
 
 const searchClient = algoliasearch('D92FAAH3QP', '5d528996ae02dbe0b55596d1736f1ed0');
