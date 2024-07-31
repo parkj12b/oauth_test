ec5e46898c733e2a40feb00468c32ee24d1bfe3e
Minseo Park <minsepar@c1r3s7.42seoul.kr>
2024-07-31 14:50:24 +0900

    .

--- js/search.js
+++ js/search.js
@@ -1,12 +1,7 @@
 const searchClient = algoliasearch('D92FAAH3QP', '5d528996ae02dbe0b55596d1736f1ed0');
-const index = searchClient.initIndex("your_index_name");
 
 const { connectAutocomplete } = instantsearch.connectors;
 // or directly use instantsearch.connectors.connectAutocomplete()
-const search = instantsearch({
-  indexName: 'demo_ecommerce',
-  searchClient,
-});
 
 // Helper for the render function
 const renderIndexListItem = ({ indexId, hits }) => `
